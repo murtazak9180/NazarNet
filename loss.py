bd5fd9cb204cdc6a7003a1f0123eef671096f720
Murtaza Kazmi <kazmimurtaza66@gmail.com>
2026-03-21 06:05:01 +0500

    implemented the ResnetEMbedding model and the constrastive loss
    Next step is to make the training loop for contrastive dataset along
    with the evaluation metrics.

---
+++ loss.py
@@ -0,0 +1,17 @@
+
+import torch
+margin = 1
+
+#Contrastive loss takes in the 2 embeddings of the given images and the label associated with it and compute 
+#loss between them
+def contrastive_loss(emb1, emb2, labels):
+    #at this point the embeddings are B X D - Batch X Dimensions(number of samples)
+    #Labels is of size D
+    #We must return average loss across the entire batch
+    dist_sq = torch.sum((emb1 - emb2)**2, dim=1)
+    dist = torch.sqrt(dist_sq)
+
+    losses = (labels * dist_sq) + ((1 - labels) * torch.clamp(margin - dist, min=0)**2)
+    total_loss = torch.mean(losses)
+
+
---
+++ model.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+resnet = models.resnet50(pretrained=True)
+
+
+class ResnetEmbedding(nn.Module):
+    def __init__(self, embedding_dim):
+        super().__init__()
+
+        self.resnet = models.resnet50(pretrained=True)
+        self.resnet.fc = nn.Linear(self.resnet.fc.in_features, embedding_dim)  #The global average pooling layer already exist in resnet50 before the fc
+
+    def forward(self, x):
+        x = self.resnet(x)
+        x = nn.Functional.normalize(x, p=2, dim=1) #L2 normalization. Makes all the points lie on a sphere, makes lerning more stable. 
+        return x 
+    
